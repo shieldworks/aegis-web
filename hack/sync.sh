785bf77e56a93e458959b76c4e6fc526b374f9bb
Volkan Özçelik <me@volkan.io>
2023-02-26 19:43:43 -0800

    updated the publish script

--- hack/sync.sh
+++ hack/sync.sh
@@ -2,6 +2,8 @@
 
 JEKYLL_ENV=production jekyll build
 
+aws s3 sync _site/ s3://aegis.ist/
+# Sync with alternate site too until S3 redirection rules propagate to all origins.
 aws s3 sync _site/ s3://aegis.z2h.dev/
 
-aws cloudfront create-invalidation --distribution-id E3LC16VB3C88K6 --paths "/*"
+aws cloudfront create-invalidation --distribution-id EZFGMY32S3BBS --paths "/*"
